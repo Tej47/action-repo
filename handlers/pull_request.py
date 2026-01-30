f5fe2e07e8348674367f57e716d15875524cf9b2
Tej47 <saitejaswiram1234@gmail.com>
2026-01-30 10:49:15 +0530

    testing pr commit

--- handlers/pull_request.py
+++ handlers/pull_request.py
@@ -4,12 +4,27 @@ from flask import jsonify
 from db import get_utc_timestamp, events, insert_event
 
 def handle_pull_request(payload):
+    action = payload.get("action")
     pull_request = payload.get("pull_request")
 
     if not pull_request:
-        return jsonify({"error": "Invalid pull request payload"}), 400
-    
+        return jsonify({"ignored": True}), 200
+
+    # Only care about opened PRs and merged PRs
+    if action not in ["opened", "closed"]:
+        return jsonify({"ignored": True}), 200
+
     is_merged = pull_request.get("merged", False)
+
+    if action == "closed" and not is_merged:
+        return jsonify({"ignored": "pr closed without merge"}), 200
+    
+    # pull_request = payload.get("pull_request")
+
+    # if not pull_request:
+    #     return jsonify({"error": "Invalid pull request payload"}), 400
+    
+    # is_merged = pull_request.get("merged", False)
 
     action_type = "MERGE" if is_merged else "PULL_REQUEST"
 
